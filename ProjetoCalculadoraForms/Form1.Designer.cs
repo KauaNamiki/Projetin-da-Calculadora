1cebc192109ce6ac18a19e24a1598cdac5aa1f4b
Kaua Namiki <kakanamiki@gmail.com>
2024-07-02 20:58:14 -0300

    calculadora forms

---
+++ ProjetoCalculadoraForms/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace ProjetoCalculadoraForms
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.cmbOperacao = new System.Windows.Forms.ComboBox();
+            this.Resultado = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(180, 22);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(46, 20);
+            this.txtNumero1.TabIndex = 0;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(180, 65);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(46, 20);
+            this.txtNumero2.TabIndex = 1;
+            // 
+            // cmbOperacao
+            // 
+            this.cmbOperacao.FormattingEnabled = true;
+            this.cmbOperacao.Location = new System.Drawing.Point(144, 97);
+            this.cmbOperacao.Name = "cmbOperacao";
+            this.cmbOperacao.Size = new System.Drawing.Size(117, 21);
+            this.cmbOperacao.TabIndex = 2;
+            // 
+            // Resultado
+            // 
+            this.Resultado.AutoSize = true;
+            this.Resultado.BackColor = System.Drawing.Color.Silver;
+            this.Resultado.Location = new System.Drawing.Point(247, 43);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(58, 13);
+            this.Resultado.TabIndex = 4;
+            this.Resultado.Text = "Resultado:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(144, 145);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(117, 23);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Primeiro Número:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Segundo Número:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Selecione a Operação:";
+            // 
+            // Calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 250);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.cmbOperacao);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Calculadora";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.ComboBox cmbOperacao;
+        private System.Windows.Forms.Label Resultado;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ProjetoCalculadoraForms/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoCalculadoraForms
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbOperacao.Items.Add("Adição");
+            cmbOperacao.Items.Add("Subtração");
+            cmbOperacao.Items.Add("Multiplicação");
+            cmbOperacao.Items.Add("Divisão");
+            cmbOperacao.SelectedIndex = 0;
+            // Estilizar os controles
+            this.BackColor = Color.LightGray;
+            cmbOperacao.BackColor = Color.White;
+            txtNumero1.BackColor = Color.White;
+            txtNumero2.BackColor = Color.White;
+            btnCalcular.BackColor = Color.FromArgb(0, 123, 255);
+            btnCalcular.ForeColor = Color.White;
+            Resultado.ForeColor = Color.Black;
+            Resultado.Font = new Font(Resultado.Font, FontStyle.Bold);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // Ajustar o tamanho dos controles quando o formulário for redimensionado
+            cmbOperacao.Width = 200;
+            txtNumero1.Width = 200;
+            txtNumero2.Width = 200;
+            btnCalcular.Width = 200;
+            Resultado.Width = 200;
+
+            // Centralizar os controles na tela
+            cmbOperacao.Left = (this.ClientSize.Width - cmbOperacao.Width) / 2;
+            txtNumero1.Left = (this.ClientSize.Width - txtNumero1.Width) / 2;
+            txtNumero2.Left = (this.ClientSize.Width - txtNumero2.Width) / 2;
+            btnCalcular.Left = (this.ClientSize.Width - btnCalcular.Width) / 2;
+            Resultado.Left = (this.ClientSize.Width - Resultado.Width) / 2;
+
+            int marginTop = 20;
+            cmbOperacao.Top = marginTop;
+            txtNumero1.Top = cmbOperacao.Bottom + marginTop;
+            txtNumero2.Top = txtNumero1.Bottom + marginTop;
+            btnCalcular.Top = txtNumero2.Bottom + marginTop;
+            Resultado.Top = btnCalcular.Bottom + marginTop;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (!decimal.TryParse(txtNumero1.Text, out decimal numero1) || !decimal.TryParse(txtNumero2.Text, out decimal numero2))
+            {
+                MessageBox.Show("Por favor, insira números válidos.");
+                return;
+            }
+
+            decimal resultado = 0;
+            string operacao = cmbOperacao.SelectedItem.ToString();
+
+            switch (operacao)
+            {
+                case "Adição":
+                    resultado = Adicionar(numero1, numero2);
+                    break;
+
+                case "Subtração":
+                    resultado = Subtrair(numero1, numero2);
+                    break;
+
+                case "Multiplicação":
+                    resultado = Multiplicar(numero1, numero2);
+                    break;
+
+                case "Divisão":
+                    if (numero2 == 0)
+                    {
+                        MessageBox.Show("Erro: Divisão por zero não é permitida.");
+                        return;
+                    }
+                    resultado = Dividir(numero1, numero2);
+                    break;
+            }
+
+            Resultado.Text = $"Resultado: {resultado:F2}";
+        }
+
+        private decimal Adicionar(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        private decimal Subtrair(decimal a, decimal b)
+        {
+            return a - b;
+        }
+
+        private decimal Multiplicar(decimal a, decimal b)
+        {
+            return a * b;
+        }
+
+        private decimal Dividir(decimal a, decimal b)
+        {
+            return a / b;
+        }
+    }
+}
